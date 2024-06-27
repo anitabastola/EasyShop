b80598d560bbecf37915c83a476978ea87d16440
student <abastola@pgh.yearup.org>
2024-06-27 11:30:17 -0400

    updated categories in the method w a string query and try catch

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -73,19 +73,61 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 
         return null;
     }
-    
+
 
     @Override
     public Category create(Category category)
     {
         // create a new category
+        String sql = "INSERT INTO categories(name, description) " + " VALUES (?, ?);";
+
+        try (Connection connection = getConnection())
+
+        {
+            PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0 ) {
+
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+
+                if (generatedKeys.next()) {
+                    int orderId = generatedKeys.getInt(1);
+
+                    return getById(orderId);
+
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
         return null;
     }
+
 
     @Override
     public void update(int categoryId, Category category)
     {
-        // update category
+        String sql = "UPDATE categories" + "SET name = ? " + " , description = ? " + " WHERE category_id = ?;";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId);
+
+            statement.executeUpdate();
+
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
